9593452aaee3a7d32da1b8ee426520832f715d99
Emad Isavi <emadesavi@gmail.com>
2025-03-06 05:36:35 +0330

    Create banner model & migration

---
+++ database/migrations/2025_03_05_234328_create_banners_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('banners', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('url');
+            $table->tinyInteger('position')->comment('0 => slideShow, 1 => bottom top right, 2 => bottom top left, 3 => bottom');
+            $table->tinyInteger('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('banners');
+    }
+};
