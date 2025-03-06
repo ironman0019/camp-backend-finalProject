580b45cd9d11b53e690c0c9f25ac70c0c155abc2
Emad Isavi <emadesavi@gmail.com>
2025-03-06 05:27:19 +0330

    Create product model & migration

---
+++ app/Models/Market/Product.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\Market;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes, Sluggable;
+
+    protected $guarded = ['id'];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+}
