95f1362ad1de28383db8ca64a2c726060b919de8
Emad Isavi <emadesavi@gmail.com>
2025-03-06 05:32:08 +0330

    Create ticket admin model & migration

---
+++ app/Models/Ticket/TicketAdmin.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Ticket;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TicketAdmin extends Model
+{
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+    
+}
