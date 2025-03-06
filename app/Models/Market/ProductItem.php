570fb8a876a7ba52d095f2366ea1fcf283831737
Emad Isavi <emadesavi@gmail.com>
2025-03-06 05:27:52 +0330

    Create product item model & migration

---
+++ app/Models/Market/ProductItem.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Market;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductItem extends Model
+{
+    use SoftDeletes;
+
+    protected $guarded = ['id'];
+    
+}
---
+++ database/migrations/2025_03_05_223648_create_product_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained('products')->onDelete('cascade')->onUpdate('cascade');
+            $table->text('file_path');
+            $table->text('file_type');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_items');
+    }
+};
