f095e79deacd1bff00381d3dcb5936367e75e469
Amirreza <Ghanavatiamir4@gmail.com>
2025-03-07 14:43:55 +0330

    modified: controllers & views - create index method and index page

--- app/Http/Controllers/Admin/Ticket/TicketCategoryController.php
+++ app/Http/Controllers/Admin/Ticket/TicketCategoryController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Admin\Ticket;
 
 use App\Http\Controllers\Controller;
+use App\Models\Ticket\TicketCategory;
 use Illuminate\Http\Request;
 
 class TicketCategoryController extends Controller
 {
     public function index()
     {
-
+        $tickets = TicketCategory::paginate(10);
+        return view('admin.ticket.ticket-category.index', compact('tickets'));
     }
 
     public function create()
--- resources/views/admin/ticket/ticket-category/index.blade.php
+++ resources/views/admin/ticket/ticket-category/index.blade.php
@@ -4,63 +4,64 @@
 
 @section('content')
 
-<section class="main-body-container">
-    <section class="main-body-container-header d-flex justify-content-between align-items-center">
-        <div>
-            <h5>
-                دسته بندی تیکت ها 
-            </h5>
-            <p>
-                در این بخش اطلاعاتی در مورد دسته بندی تیکت ها به شما داده می شود
-            </p>
-        </div>
-        <div>
-            <a href="#" class="btn btn-success">ساخت</a>
-        </div>
-    </section>
-    <section class="body-content">
+    <section class="main-body-container">
+        <section class="main-body-container-header d-flex justify-content-between align-items-center">
+            <div>
+                <h5>
+                    دسته بندی تیکت ها
+                </h5>
+                <p>
+                    در این بخش اطلاعاتی در مورد دسته بندی تیکت ها به شما داده می شود
+                </p>
+            </div>
+            <div>
+                <a href="#" class="btn btn-success">ساخت</a>
+            </div>
+        </section>
+        <section class="body-content">
 
-        <table class="table">
-            <thead class="table-info">
+            <table class="table">
+                <thead class="table-info">
                 <tr>
                     <th scope="col">#</th>
                     <th scope="col">نام</th>
                     <th scope="col">تنظیمات</th>
                 </tr>
-            </thead>
-            <tbody>
-                
-                <tr>
-                    <th>#</th>
-                    <td>#</td>
-                    <td>
-                        <div class="d-flex align-items-center">
-                            <div class="mx-2">
-                                <form action="#" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit"
-                                        class="text-danger">
-                                        <i class="fa fa-trash"></i>
-                                    </button>
-                                </form>
+                </thead>
+                @forelse($tickets as $ticket)
+                    <tbody>
+
+                    <tr>
+                        <th>{{ $loop->iteration }}</th>
+                        <td>{{ $ticket->name }}</td>
+                        <td>
+                            <div class="d-flex align-items-center">
+                                <div class="mx-2">
+                                    <form action="{{ route('admin.tickets.ticket-category.destroy', $ticket) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit"
+                                                class="text-danger">
+                                            <i class="fa fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </div>
+                                <div class="mx-2">
+                                    <a href="{{ route('admin.tickets.ticket-category.edit', $ticket) }}"
+                                       class="text-warning">
+                                        <i class="fa fa-edit"></i>
+                                    </a>
+                                </div>
                             </div>
-                            <div class="mx-2">
-                                <a href="#"
-                                    class="text-warning">
-                                    <i class="fa fa-edit"></i>
-                                </a>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
-                
+                        </td>
+                    </tr>
+                    @empty @endforelse
 
 
-            </tbody>
-        </table>
+                    </tbody>
+            </table>
 
+        </section>
     </section>
-</section>
 
 @endsection
