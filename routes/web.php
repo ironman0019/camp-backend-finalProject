b7719b9698126f4fd6af2f475076b57d08f6e66b
Emad Isavi <emadesavi@gmail.com>
2025-04-03 04:34:57 +0330

    Add admin market product-items routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\Ticket\TicketController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashbordController;
 use App\Http\Controllers\Admin\Market\ProductController;
+use App\Http\Controllers\Admin\Market\ProductItemController;
 use App\Http\Controllers\Home\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -49,6 +50,15 @@ Route::prefix('admin')->middleware([])->name('admin.')->group(function() {
         Route::get('product/file/download/{filePath}', [ProductController::class, 'downloadFile'])->where('filePath', '.*')->name('product.download-file');
         Route::get('product/change/discount-status/{product}', [ProductController::class, 'discountStatus'])->name('product.change.discount-status');
         Route::put('product/add-discount/{product}', [ProductController::class, 'addDiscount'])->name('product.add-discount');
+        // Product_items
+        Route::get('product-items/index/{product}', [ProductItemController::class, 'index'])->name('product-items.index');
+        Route::get('product-items/create/{product}', [ProductItemController::class, 'create'])->name('product-items.create');
+        Route::get('product-items/edit/{productItem}/{product}', [ProductItemController::class, 'edit'])->name('product-items.edit');
+        Route::post('product-items/store/{product}', [ProductItemController::class, 'store'])->name('product-items.store');
+        Route::put('product-items/update/{productItem}/{product}', [ProductItemController::class, 'store'])->name('product-items.update');
+        Route::delete('product-items/delete/{productItem}', [ProductItemController::class, 'destroy'])->name('product-items.destroy');
+        Route::get('product-items/file/download/{filePath}', [ProductItemController::class, 'downloadFile'])->where('filePath', '.*')->name('product-items.download-file');
+        Route::put('product-items/rename-file-item/{productItem}', [ProductItemController::class, 'renameFile'])->name('product-items.rename-file');
         
 
     });
