8dde6f76ebe05524875c941bbdcd7133ec36f624
Emad Isavi <emadesavi@gmail.com>
2025-04-05 18:54:28 +0330

    Create email otp blade file

---
+++ resources/views/emails/otp.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <title>کد ورود</title>
+</head>
+<body>
+    <h2>کد ورود شما:</h2>
+    <p style="font-size: 24px; font-weight: bold;">{{ $otpCode }}</p>
+    <p>این کد فقط برای ورود به حساب شما معتبر است و تا چند دقیقه دیگر منقضی می‌شود.</p>
+</body>
+</html>
