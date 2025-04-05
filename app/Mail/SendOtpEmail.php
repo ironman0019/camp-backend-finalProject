7f6ed08ea4b8332a318b5a18ff156ce399741d92
Emad Isavi <emadesavi@gmail.com>
2025-04-05 18:54:07 +0330

    Create mail class for sending otp code

---
+++ app/Mail/SendOtpEmail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendOtpEmail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public $otpCode;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($otpCode)
+    {
+        $this->otpCode = $otpCode;
+    }
+
+    public function build()
+    {
+        return $this->subject('کد ورود یکبار مصرف شما')
+                    ->view('emails.otp');
+    }
+
+}
