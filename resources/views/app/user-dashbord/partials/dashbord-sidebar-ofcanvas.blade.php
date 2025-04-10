e0431fb40f053def1ba65b98e712ede7cb34d70d
Emad Isavi <emadesavi@gmail.com>
2025-04-11 01:39:40 +0330

    Create user dashbord sidebar partial blade file

---
+++ resources/views/app/user-dashbord/partials/dashbord-sidebar-ofcanvas.blade.php
@@ -0,0 +1,30 @@
+<div class="offcanvas offcanvas-bottom" id="offcanvasUserNav" tabindex="-1" aria-labelledby="offcanvasUserNavLabel"
+style="--bs-offcanvas-height:75vh">
+<div class="offcanvas-header">
+    <h5 class="offcanvas-title" id="offcanvasUserNavLabel">منو پروفایل</h5>
+    <button class="btn-close" type="button" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+</div>
+<div class="offcanvas-body p-0">
+    <nav class="h-100 profile-sidebar-nav bg-light-subtle p-3 rounded-bottom-4"><a
+            class="btn btn-lg nt-fw-500 profile-sidebar-link active" href="javascript:;"><i
+                class="ti ti-home-2 fs-3"></i>خلاصه فعالیت ها</a><a
+            class="btn btn-lg nt-fw-500 profile-sidebar-link" href="user-orders.html"><i
+                class="ti ti-shopping-bag fs-3"></i>سفارش ها</a><a
+            class="btn btn-lg nt-fw-500 profile-sidebar-link" href="user-lists.html"><i
+                class="ti ti-heart fs-3"></i>لیست های من</a><a class="btn btn-lg nt-fw-500 profile-sidebar-link"
+            href="user-comments.html"><i class="ti ti-message fs-3"></i>دیدگاه ها</a><a
+            class="btn btn-lg nt-fw-500 profile-sidebar-link" href="user-addresses.html"><i
+                class="ti ti-address-book fs-3"></i>آدرس ها</a><a
+            class="btn btn-lg nt-fw-500 profile-sidebar-link" href="user-history.html"><i
+                class="ti ti-history fs-3"></i>بازدید های اخیر</a><a
+            class="btn btn-lg nt-fw-500 profile-sidebar-link" href="user-info.html"><i
+                class="ti ti-user fs-3"></i>اطلاعات حساب کاربری</a>
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button
+                    class="btn btn-lg nt-fw-500 profile-sidebar-link" type="submit"><i
+                        class="ti ti-logout fs-3"></i>خروج</button>
+                </form>
+        </nav>
+</div>
+</div>
---
+++ resources/views/app/user-dashbord/partials/dashbord-sidebar.blade.php
@@ -0,0 +1,71 @@
+<div class="col-12 col-md-3">
+    <!-- sidebar-->
+    <aside class="profile-sidebar">
+        <!-- header-->
+        <header class="profile-sidebar-header text-bg-primary bg-gradient">
+            <div class="nt-flex-start-center gap-4 mb-4">
+                <!-- avatar-->
+                <div class="profile-sidebar-avatar"><img src="{{ asset(auth()->user()->profile_image) }}" alt=""
+                        width="75" /></div>
+                <div class="nt-flex-column">
+                    <!-- name-->
+                    <div class="fs-4 nt-fw-bold">{{ auth()->user()->name }}</div>
+                    <!-- number-->
+                    <div class="text-light">{{ auth()->user()->mobile ? auth()->user()->mobile : auth()->user()->email }}</div>
+                    <!-- edit--><a class="btn btn-sm btn-light" href="{{ route('user.profile.edit', auth()->user()->id) }}"><i
+                            class="ti ti-user-edit fs-5"></i>ویرایش حساب</a>
+                </div>
+            </div>
+            <ul class="list-group w-100">
+                <!-- wallet-->
+                <li class="list-group-item nt-flex-between-start">
+                    <div class="nt-flex-column py-1">
+                        <div class="text-body-emphasis nt-flex nt-fw-500 mb-2"><i
+                                class="ti ti-wallet fs-4 text-primary"></i>کیف پول</div><a
+                            class="icon-link icon-link-hover small" href="#">افزایش موجودی<i
+                                class="ti ti-chevron-left bi"></i></a>
+                    </div>
+                    <div class="nt-flex">
+                        <div class="text-body-emphasis nt-flex-start-center py-1">
+                            <div class="nt-fw-500">۰</div>
+                            <div class="small">تومان</div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </header>
+        <!-- mobile nav-->
+        <div class="d-grid d-lg-none p-4">
+            <button class="btn btn-lg btn-light" type="button" data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasUserNav" aria-controls="offcanvasUserNav"><i
+                    class="ti ti-menu-2 fs-2"></i>
+                <div class="nt-fw-500">منو پروفایل</div>
+            </button>
+        </div>
+        <div class="d-none d-lg-block">
+            <!-- sidebar nav-->
+            <nav class="profile-sidebar-nav bg-body"><a
+                    class="btn btn-lg nt-fw-500 profile-sidebar-link active" href="javascript:;"><i
+                        class="ti ti-home-2 fs-3"></i>خلاصه فعالیت ها</a><a
+                    class="btn btn-lg nt-fw-500 profile-sidebar-link" href="user-orders.html"><i
+                        class="ti ti-shopping-bag fs-3"></i>سفارش ها</a><a
+                    class="btn btn-lg nt-fw-500 profile-sidebar-link" href="user-lists.html"><i
+                        class="ti ti-heart fs-3"></i>لیست های من</a><a
+                    class="btn btn-lg nt-fw-500 profile-sidebar-link" href="user-comments.html"><i
+                        class="ti ti-message fs-3"></i>دیدگاه ها</a><a
+                    class="btn btn-lg nt-fw-500 profile-sidebar-link" href="user-addresses.html"><i
+                        class="ti ti-address-book fs-3"></i>آدرس ها</a><a
+                    class="btn btn-lg nt-fw-500 profile-sidebar-link" href="user-history.html"><i
+                        class="ti ti-history fs-3"></i>بازدید های اخیر</a><a
+                    class="btn btn-lg nt-fw-500 profile-sidebar-link" href="user-info.html"><i
+                        class="ti ti-user fs-3"></i>اطلاعات حساب کاربری</a>
+                        <form action="{{ route('logout') }}" method="POST">
+                            @csrf
+                            <button
+                            class="btn btn-lg nt-fw-500 profile-sidebar-link" type="submit"><i
+                                class="ti ti-logout fs-3"></i>خروج</button>
+                        </form>
+            </nav>
+        </div>
+    </aside>
+</div>
