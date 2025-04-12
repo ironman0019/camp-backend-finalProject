4cb1a0e52e7c9f6f4f402e17284bc5860923a525
Emad Isavi <emadesavi@gmail.com>
2025-04-12 12:38:18 +0330

    Add menus and product categories and tags to all view blade pages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Market\ProductCategory;
+use App\Models\Menu;
+use App\Models\Tag;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -22,6 +26,25 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrapFour();
+
+        View::composer('*', function($view) {
+            $menus = Menu::whereNull('parent_id')->get();
+            $view->with('menus', $menus);
+        });
+
+        View::composer('*', function($view) {
+            $productCategories = ProductCategory::with(['products' => function ($query) {
+                $query->latest()->take(10);
+            }])->get();
+            $view->with('productCategories', $productCategories);
+        });
+
+        View::composer('*', function($view) {
+            $tags = Tag::all();
+            $view->with('tags', $tags);
+        });
+
+        
         // auth()->loginUsingId(1);
         // Model::preventsLazyLoading();
     }
