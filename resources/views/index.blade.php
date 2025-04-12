bbc2cd696f1ec11db4c345903c5d98b10d112cc3
Emad Isavi <emadesavi@gmail.com>
2025-04-12 12:39:08 +0330

    Make button links and tags section dynamic

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -422,152 +422,40 @@
     <section class="container-fluid wrapper mb-5">
         <div class="row g-4 py-4">
             <!-- item-->
-            <div class="col-12 col-md-6 col-lg-4"><a class="index-buttonLink btn btn-light" href="#">
+            @foreach($productCategories as $productCategory)
+            <div class="col-12 col-md-6 col-lg-4">
+                <a class="index-buttonLink btn btn-light" href="#">
                     <div class="nt-flex-column">
                         <div class="fs-4 nt-fw-bold mb-4">پرفروش ترین های</div>
                         <div class="nt-flex-start-center mb-3"><i class="ti ti-caret-left-filled fs-3 text-primary"></i>
-                            <div class="fs-2">قالب سایت</div>
+                            <div class="fs-2">{{ $productCategory->name }}</div>
                         </div>
                         <div class="nt-flex-start-center">
                             <div class="btn btn-lg btn-primary rounded-5" style="border-top-right-radius: 0 !important">
                                 <div class="fs-6">خرید</div><i class="ti ti-arrow-left fs-5"></i>
                             </div>
                         </div>
-                    </div><img src="{{ asset('assets/img/pages/index/card/phone.png') }}" alt="" width="125" />
-                </a></div>
-            <!-- item-->
-            <div class="col-12 col-md-6 col-lg-4"><a class="index-buttonLink btn btn-light" href="#">
-                    <div class="nt-flex-column">
-                        <div class="fs-4 nt-fw-bold mb-4">پرفروش ترین های</div>
-                        <div class="nt-flex-start-center mb-3"><i class="ti ti-caret-left-filled fs-3 text-primary"></i>
-                            <div class="fs-2">قالب وردپرسی</div>
-                        </div>
-                        <div class="nt-flex-start-center">
-                            <div class="btn btn-lg btn-primary rounded-5" style="border-top-right-radius: 0 !important">
-                                <div class="fs-6">خرید</div><i class="ti ti-arrow-left fs-5"></i>
-                            </div>
-                        </div>
-                    </div><img src="{{ asset('assets/img/pages/index/card/tablet.png') }}" alt="" width="125" />
-                </a></div>
-            <!-- item-->
-            <div class="col-12 col-md-6 col-lg-4"><a class="index-buttonLink btn btn-light" href="#">
-                    <div class="nt-flex-column">
-                        <div class="fs-4 nt-fw-bold mb-4">پرفروش ترین های</div>
-                        <div class="nt-flex-start-center mb-3"><i class="ti ti-caret-left-filled fs-3 text-primary"></i>
-                            <div class="fs-2">کتاب الکترونیک</div>
-                        </div>
-                        <div class="nt-flex-start-center">
-                            <div class="btn btn-lg btn-primary rounded-5" style="border-top-right-radius: 0 !important">
-                                <div class="fs-6">خرید</div><i class="ti ti-arrow-left fs-5"></i>
-                            </div>
-                        </div>
-                    </div><img src="{{ asset('assets/img/pages/index/card/laptop.png') }}" alt="" width="125" />
-                </a></div>
-            <!-- item-->
-            <div class="col-12 col-md-6"><a class="index-buttonLink btn btn-light" href="#">
-                    <div class="nt-flex-column">
-                        <div class="fs-4 nt-fw-bold mb-4">پرفروش ترین های</div>
-                        <div class="nt-flex-start-center mb-3"><i class="ti ti-caret-left-filled fs-3 text-primary"></i>
-                            <div class="fs-2">دوره آموزشی</div>
-                        </div>
-                        <div class="nt-flex-start-center">
-                            <div class="btn btn-lg btn-primary rounded-5" style="border-top-right-radius: 0 !important">
-                                <div class="fs-6">خرید</div><i class="ti ti-arrow-left fs-5"></i>
-                            </div>
-                        </div>
-                    </div><img src="{{ asset('assets/img/pages/index/card/meta.png') }}" alt="" width="200" />
-                </a></div>
-            <!-- item-->
-            <div class="col-12 col-md-6"><a class="index-buttonLink btn btn-light" href="#">
-                    <div class="nt-flex-column">
-                        <div class="fs-4 nt-fw-bold mb-4">پرفروش ترین های</div>
-                        <div class="nt-flex-start-center mb-3"><i class="ti ti-caret-left-filled fs-3 text-primary"></i>
-                            <div class="fs-2">پلاگین های فیگما</div>
-                        </div>
-                        <div class="nt-flex-start-center">
-                            <div class="btn btn-lg btn-primary rounded-5" style="border-top-right-radius: 0 !important">
-                                <div class="fs-6">خرید</div><i class="ti ti-arrow-left fs-5"></i>
-                            </div>
-                        </div>
-                    </div><img src="{{ asset('assets/img/pages/index/card/console.png') }}" alt="" width="125" />
-                </a></div>
+                    </div><img src="https://picsum.photos/id/237/125" alt="" width="125" />
+                </a>
+            </div>
+            @endforeach
         </div>
     </section>
-    <!-- categories slider-->
+    <!-- tags slider-->
     <section class="container-fluid wrapper mb-5">
         <div class="swiper py-5 px-1"
             data-swiper-options="{ &quot;loop&quot;:true, &quot;autoplay&quot;:{&quot;delay&quot;: 3000}, &quot;speed&quot;:1000, &quot;slidesPerView&quot;:1, &quot;spaceBetween&quot;:30, &quot;breakpoints&quot;:{ &quot;480&quot;:{&quot;slidesPerView&quot;:3},&quot;640&quot;:{&quot;slidesPerView&quot;:6},&quot;720&quot;:{&quot;slidesPerView&quot;:8} } }">
             <div class="swiper-wrapper">
                 <!-- slide-->
-                <div class="swiper-slide h-auto"><a
+                @foreach($tags as $tag)
+                <div class="swiper-slide h-auto">
+                    <a
                         class="h-100 link-body-emphasis nt-flex-column-center-center border bg-body rounded-5 p-3"
-                        href="#"><img src="{{ asset('assets/img/pages/index/card/laptop.png') }}" alt="" width="100" />
-                        <div class="fs-4 nt-fw-500">لپ‌تاپ</div>
-                        <div class="text-body-secondary">LapTop</div>
-                    </a></div>
-                <!-- slide-->
-                <div class="swiper-slide h-auto"><a
-                        class="h-100 link-body-emphasis nt-flex-column-center-center border bg-body rounded-5 p-3"
-                        href="#"><img src="{{ asset('assets/img/pages/index/card/phone.png') }}" alt="" width="100" />
-                        <div class="fs-4 nt-fw-500">گوشی موبایل</div>
-                        <div class="text-body-secondary">Phone</div>
-                    </a></div>
-                <!-- slide-->
-                <div class="swiper-slide h-auto"><a
-                        class="h-100 link-body-emphasis nt-flex-column-center-center border bg-body rounded-5 p-3"
-                        href="#"><img src="{{ asset('assets/img/pages/index/card/tablet.png') }}" alt="" width="100" />
-                        <div class="fs-4 nt-fw-500">تبلت</div>
-                        <div class="text-body-secondary">Tablet</div>
-                    </a></div>
-                <!-- slide-->
-                <div class="swiper-slide h-auto"><a
-                        class="h-100 link-body-emphasis nt-flex-column-center-center border bg-body rounded-5 p-3"
-                        href="#"><img src="{{ asset('assets/img/pages/index/card/laptop.png') }}" alt="" width="100" />
-                        <div class="fs-4 nt-fw-500">لپ‌تاپ</div>
-                        <div class="text-body-secondary">LapTop</div>
-                    </a></div>
-                <!-- slide-->
-                <div class="swiper-slide h-auto"><a
-                        class="h-100 link-body-emphasis nt-flex-column-center-center border bg-body rounded-5 p-3"
-                        href="#"><img src="{{ asset('assets/img/pages/index/card/console.png') }}" alt="" width="100" />
-                        <div class="fs-4 nt-fw-500">کنسول</div>
-                        <div class="text-body-secondary">Console</div>
-                    </a></div>
-                <!-- slide-->
-                <div class="swiper-slide h-auto"><a
-                        class="h-100 link-body-emphasis nt-flex-column-center-center border bg-body rounded-5 p-3"
-                        href="#"><img src="{{ asset('assets/img/pages/index/card/meta.png') }}" alt="" width="100" />
-                        <div class="fs-4 nt-fw-500">واقعیت مجازی</div>
-                        <div class="text-body-secondary">VR headset</div>
-                    </a></div>
-                <!-- slide-->
-                <div class="swiper-slide h-auto"><a
-                        class="h-100 link-body-emphasis nt-flex-column-center-center border bg-body rounded-5 p-3"
-                        href="#"><img src="{{ asset('assets/img/pages/index/card/phone.png') }}" alt="" width="100" />
-                        <div class="fs-4 nt-fw-500">گوشی موبایل</div>
-                        <div class="text-body-secondary">Phone</div>
-                    </a></div>
-                <!-- slide-->
-                <div class="swiper-slide h-auto"><a
-                        class="h-100 link-body-emphasis nt-flex-column-center-center border bg-body rounded-5 p-3"
-                        href="#"><img src="{{ asset('assets/img/pages/index/card/tablet.png') }}" alt="" width="100" />
-                        <div class="fs-4 nt-fw-500">تبلت</div>
-                        <div class="text-body-secondary">Tablet</div>
-                    </a></div>
-                <!-- slide-->
-                <div class="swiper-slide h-auto"><a
-                        class="h-100 link-body-emphasis nt-flex-column-center-center border bg-body rounded-5 p-3"
-                        href="#"><img src="{{ asset('assets/img/pages/index/card/console.png') }}" alt="" width="100" />
-                        <div class="fs-4 nt-fw-500">کنسول</div>
-                        <div class="text-body-secondary">Console</div>
-                    </a></div>
-                <!-- slide-->
-                <div class="swiper-slide h-auto"><a
-                        class="h-100 link-body-emphasis nt-flex-column-center-center border bg-body rounded-5 p-3"
-                        href="#"><img src="{{ asset('assets/img/pages/index/card/meta.png') }}" alt="" width="100" />
-                        <div class="fs-4 nt-fw-500">واقعیت مجازی</div>
-                        <div class="text-body-secondary">VR headset</div>
-                    </a></div>
+                        href="#">
+                        <div class="fs-4 nt-fw-500">{{ $tag->name }}</div>
+                    </a>
+                </div>
+                @endforeach
             </div>
         </div>
     </section>
