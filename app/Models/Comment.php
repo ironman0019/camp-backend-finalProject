4a34af517d5410ac58bf8231ae1307263d2045eb
Emad Isavi <emadesavi@gmail.com>
2025-04-12 20:00:30 +0330

    Add needed relations in comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -10,4 +10,21 @@ class Comment extends Model
     use SoftDeletes;
 
     protected $guarded = ['id'];
+
+    // Relation with user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo($this, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany($this, 'parent_id');
+    }
+    
 }
