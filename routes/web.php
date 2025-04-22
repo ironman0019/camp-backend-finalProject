6c14c28357f37fff2772fb55d4df1e83c1d2844b
Emad Isavi <emadesavi@gmail.com>
2025-04-22 05:12:37 +0330

    Add user dashbord user-orders and user-orders-detail routes

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,8 @@ Route::middleware(['auth'])->group(function() {
     // user dashbord routes
     Route::get('user/dashbord/index', [UserDashbordController::class, 'index'])->name('user.dashbord.index');
     Route::get('user/dashbord/user-info', [UserDashbordController::class, 'userInfo'])->name('user.dashbord.user-info');
+    Route::get('user/dashbord/user-orders', [UserDashbordController::class, 'userOrders'])->name('user.dashbord.user-orders');
+    Route::get('user/dashbord/user-orders-detail/{order}', [UserDashbordController::class, 'userOrdersDetail'])->name('user.dashbord.user-orders-detail');
     Route::get('user/profile/edit/{id}', [UserProfileController::class, 'edit'])->name('user.profile.edit');
     Route::put('user/profile/update/{user}', [UserProfileController::class, 'update'])->name('user.profile.update');
 
