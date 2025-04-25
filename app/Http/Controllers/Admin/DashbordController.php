77d675d3b0293d98ad27ee72ce7dc3a224000ab1
Emad Isavi <emadesavi@gmail.com>
2025-04-25 03:56:21 +0330

    Modified: change admin dashbord controller from invoke controller to normal controller and add getSaleStats function for charts in admin index page

--- app/Http/Controllers/Admin/DashbordController.php
+++ app/Http/Controllers/Admin/DashbordController.php
@@ -2,16 +2,32 @@
 
 namespace App\Http\Controllers\Admin;
 
+
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Market\Order;
 
 class DashbordController extends Controller
 {
     /**
-     * Handle the incoming request.
+     * Return index page
      */
-    public function __invoke(Request $request)
+    public function index()
     {
         return view('admin.index');
+    }
+
+    public function getSaleStats()
+    {
+        $today = (int) Order::whereDate('created_at', today())->where('peyment_status', 1)->where('order_status', 2)->sum('order_total_discount_amount');
+        $thisMonth = (int) Order::whereMonth('created_at', now()->month)->where('peyment_status', 1)->where('order_status', 2)->sum('order_total_discount_amount');
+        $thisYear = (int) Order::whereYear('created_at', now()->year)->where('peyment_status', 1)->where('order_status', 2)->sum('order_total_discount_amount');
+        $totalSales = (int) Order::where('peyment_status', 1)->where('order_status', 2)->sum('order_total_discount_amount');
+
+        return response()->json([
+            'today' => $today,
+            'thisMonth' => $thisMonth,
+            'thisYear' => $thisYear,
+            'total' => $totalSales,
+        ]);
     }
 }
