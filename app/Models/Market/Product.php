3f4e90aca7c668b823e2c12cfa21566d1ec5c2c7
Emad Isavi <emadesavi@gmail.com>
2025-04-27 02:24:20 +0330

    Add scopeFilter function for filter and sort products based on different situation in product model

--- app/Models/Market/Product.php
+++ app/Models/Market/Product.php
@@ -45,4 +45,69 @@ class Product extends Model
         return $this->hasMany(ProductItem::class);
     }
 
+    // Search filter
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when(filled($filters['search'] ?? null), function ($q) use ($filters) {
+            $q->where(function ($subquery) use ($filters) {
+                $subquery->where('name', 'like', '%' . $filters['search'] . '%')
+                         ->orWhere('description', 'like', '%' . $filters['search'] . '%');
+            });
+        });
+    
+        $query->when(filled($filters['product-category'] ?? null), function ($q) use ($filters) {
+            $q->whereRelation('productCategory', 'name', '=', $filters['product-category']);
+        });
+
+        $query->when(filled($filters['tag'] ?? null), function ($q) use ($filters) {
+            $q->whereHas('tags', function ($q) use ($filters) {
+                $q->where('name', $filters['tag']);
+            });
+        });
+    
+        $query->when(isset($filters['marketable']) && $filters['marketable'] == 1, function ($q) {
+            $q->where('marketable', 1);
+        });
+    
+        $query->when(isset($filters['image-products']) && $filters['image-products'] == 1, function ($q) {
+            $q->whereNotNull('image');
+        });
+    
+        if (filled($filters['start-price'] ?? null) || filled($filters['end-price'] ?? null)) {
+            $query->when(filled($filters['start-price']), function ($q) use ($filters) {
+                $q->where('price', '>=', $filters['start-price']);
+            });
+        
+            $query->when(filled($filters['end-price']), function ($q) use ($filters) {
+                $q->where('price', '<=', $filters['end-price']);
+            });
+        }
+
+        // Sort
+        if (filled($filters['sort'] ?? null)) {
+            switch ($filters['sort']) {
+                case 'expensive':
+                    $query->orderBy('price', 'desc');
+                    break;
+                case 'cheap':
+                    $query->orderBy('price', 'asc');
+                    break;
+                case 'newest':
+                    $query->orderBy('created_at', 'desc');
+                    break;
+                case 'most-viewed':
+                    $query->orderBy('view_count', 'desc');
+                    break;
+                case 'sold-number':
+                    $query->orderBy('sold_number', 'desc');
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            $query->latest();
+        }
+    }
+    
+
 }
