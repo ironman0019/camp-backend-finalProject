1c5cc5efa0b5ba087bb527463dd5c76cb2805d31
Emad Isavi <emadesavi@gmail.com>
2025-05-08 11:24:17 +0330

    Modified: pass banners to index blade file

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -7,6 +7,7 @@ use App\Models\Comment;
 use Illuminate\Http\Request;
 use App\Models\Market\Product;
 use App\Http\Controllers\Controller;
+use App\Models\Banner;
 use App\Models\Market\Order;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,7 +18,11 @@ class HomeController extends Controller
         $mostViewedProducts = Product::where('marketable', 1)->orderBy('view_count', 'desc')->take(8)->get();
         $discountProducts = Product::where('marketable', 1)->where('discount_status', 1)->take(10)->get();
         $tags = Tag::all();
-        return view('index', compact('mostViewedProducts', 'discountProducts', 'tags'));
+        $slideBanners = Banner::latest()->where('status', 1)->where('position', 0)->take(5)->get();
+        $bottomBanners = Banner::latest()->where('status', 1)->where('position', 3)->take(4)->get();
+        $bottomTopLeftBanner = Banner::where('status', 1)->where('position', 2)->first();
+        $bottomTopRightBanner = Banner::where('status', 1)->where('position', 1)->first();
+        return view('index', compact('mostViewedProducts', 'discountProducts', 'tags', 'slideBanners', 'bottomBanners', 'bottomTopLeftBanner', 'bottomTopRightBanner'));
     }
 
     public function product(Product $product)
