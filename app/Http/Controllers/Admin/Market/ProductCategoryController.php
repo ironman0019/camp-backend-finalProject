b3b65d06be20b08babbe404f5dfef909bc849490
Emad Isavi <emadesavi@gmail.com>
2025-05-09 18:11:14 +0330

    Create admin market product category controller and implement CRUD functions

---
+++ app/Http/Controllers/Admin/Market/ProductCategoryController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Market;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Product\StoreProductCategoryRequest;
+use App\Http\Requests\Admin\Product\UpdateProductCategoryRequest;
+use App\Models\Market\ProductCategory;
+
+class ProductCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $categories = ProductCategory::all();
+        return view('admin.market.product-category.index', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('admin.market.product-category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreProductCategoryRequest $request)
+    {
+        $inputs = $request->all();
+        ProductCategory::create($inputs);
+        return to_route('admin.market.product-category.index')->with('swal-success', 'دسته بندی با موفقیت ساخته شد');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductCategory $productCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductCategory $productCategory)
+    {
+        return view('admin.market.product-category.edit', compact('productCategory'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateProductCategoryRequest $request, ProductCategory $productCategory)
+    {
+        $inputs = $request->all();
+        $productCategory->update($inputs);
+        return to_route('admin.market.product-category.index')->with('swal-success', 'دسته بندی با موفقیت ویرایش شد');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductCategory $productCategory)
+    {
+        $productCategory->delete();
+        return back()->with('swal-success', 'دسته بندی با موفقیت حذف شد');
+    }
+}
