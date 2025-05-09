49f5abbb295aa92fdec3c4e3e54cf9c295c7f3cc
Emad Isavi <emadesavi@gmail.com>
2025-05-09 21:06:01 +0330

    Create admin panel content menu controller and implement CRUD fucntions

---
+++ app/Http/Controllers/Admin/Content/MenuController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Content;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Content\StoreMenuRequest;
+use App\Http\Requests\Admin\Content\UpdateMenuRequest;
+use App\Models\Menu;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $adminPanelMenus = Menu::all();
+        return view('admin.content.menu.index', compact('adminPanelMenus'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $parents = Menu::where('parent_id', null)->get();
+        return view('admin.content.menu.create', compact('parents'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMenuRequest $request)
+    {
+        $inputs = $request->all();
+        
+        Menu::create($inputs);
+        return to_route('admin.content.menu.index')->with('swal-success', 'منو با موفقیت ساخته شد');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Menu $menu)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Menu $menu)
+    {
+        $parents = Menu::where('parent_id', null)->get()->except($menu->id);
+        return view('admin.content.menu.edit', compact('parents', 'menu'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMenuRequest $request, Menu $menu)
+    {
+        $inputs = $request->all();
+
+        $menu->update($inputs);
+        return to_route('admin.content.menu.index')->with('swal-success', 'منو با موفقیت ویرایش شد');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Menu $menu)
+    {
+        $menu->delete();
+        return back()->with('swal-success', 'منو با موفقیت حذف شد');
+    }
+}
