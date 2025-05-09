52ee9ac7021ae8115ae6ce0da5a63dd20555f25c
Emad Isavi <emadesavi@gmail.com>
2025-05-09 19:11:03 +0330

    Create coupan store & update requests and add validation rules

---
+++ app/Http/Requests/Admin/Coupan/UpdateCoupanRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Admin\Coupan;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateCoupanRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'code' => 'required|max:120|min:2|regex:/^[a-zA-Z0-9\., ]+$/u',
+            'amount' => 'required|numeric',
+            'amount_type' => 'required|in:0,1',
+            'discount_ceiling' => 'required_if:amount_type,0|nullable|numeric',
+            'start_date' => 'required|numeric',
+            'end_date' => 'required|numeric',
+            'status' => 'required|in:0,1'
+        ];
+    }
+}
