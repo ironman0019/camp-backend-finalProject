dcb649b6cefa4f4ad5a5491afc9faeb7b8243871
Emad Isavi <emadesavi@gmail.com>
2025-05-10 16:37:01 +0330

    Implement index, store and download file methods in dashbord user ticket controller

--- app/Http/Controllers/Home/UserTicketController.php
+++ app/Http/Controllers/Home/UserTicketController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers\Home;
 
-use App\Http\Controllers\Controller;
-use App\Services\FileUploadService;
 use Illuminate\Http\Request;
+use App\Models\Ticket\Ticket;
+use App\Models\Ticket\TicketFile;
+use App\Services\FileUploadService;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Ticket\TicketCategory;
+use Illuminate\Support\Facades\Storage;
 
 class UserTicketController extends Controller
 {
@@ -13,7 +18,9 @@ class UserTicketController extends Controller
      */
     public function index()
     {
-        return view('app.user-dashbord.user-tickets');
+        $tickets = Ticket::with('file')->where('user_id', Auth::user()->id)->get();
+        $ticketCategories = TicketCategory::all();
+        return view('app.user-dashbord.user-tickets', compact('tickets', 'ticketCategories'));
     }
 
 
@@ -22,7 +29,56 @@ class UserTicketController extends Controller
      */
     public function store(Request $request, FileUploadService $fileUploadService)
     {
-        //
+        $inputs = $request->validate([
+            'category_id' => 'required|exists:ticket_categories,id|integer',
+            'subject' => 'required|string|min:4|max:255',
+            'body' => 'required|string|min:4|max:2000',
+            'file' => 'mimes:png,jpg,jpeg,pdf,txt,doc,docx,zip,rar|nullable'
+        ]);
+
+        $user = Auth::user();
+
+        $ticket = Ticket::create([
+            'reference_id' => 1,
+            'user_id' => $user->id,
+            'category_id' => $inputs['category_id'],
+            'subject' => $inputs['subject'],
+            'body' => $inputs['body']
+        ]);
+
+        if ($request->hasFile('file')) {
+            $result = $fileUploadService->uploadFile($request->file('file'));
+            if ($result === false) {
+                return back()->with('swal-error', 'خطا در آپلود فایل');
+            }
+            $inputs['file_path'] = $result['path'];
+            $inputs['file_type'] = $result['type'];
+            $inputs['file_size'] = $result['size'];
+
+            $ticketFile = TicketFile::create([
+                'user_id' => $user->id,
+                'ticket_id' => $ticket->id,
+                'file_path' => $inputs['file_path'],
+                'file_size' => $inputs['file_size'],
+                'file_type' => $inputs['file_type']
+            ]);
+
+        }
+
+        return back()->with('success', 'تیکت با موفقیت ثبت شد');
+
+
+    }
+
+    // Download file 
+    public function downloadFile(TicketFile $ticketFile)
+    {
+        $filePath = $ticketFile->file_path;
+        if (!Storage::exists($filePath)) {
+            return 'file not found';
+        }
+
+        return Storage::download($filePath);
     }
 
 }
