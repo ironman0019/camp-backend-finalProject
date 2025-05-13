38ae41f9f376c87da694c01c1add9df90639bfc3
Emad Isavi <emadesavi@gmail.com>
2025-05-13 22:57:38 +0330

    Create user wallet controller and implement methods for increaseing wallet amount

---
+++ app/Http/Controllers/Home/UserWalletController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Home;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use App\Models\Market\Peyment;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Models\User\WalletPeyment;
+use Illuminate\Support\Facades\Auth;
+
+class UserWalletController extends Controller
+{
+    public function create()
+    {
+        return view('app.user-dashbord.user-wallet-form');
+    }
+
+    public function store(Request $request)
+    {
+
+        $inputs = $request->validate([
+            'amount' => 'required|numeric'
+        ]);
+
+        $inputs['gateway'] = 'زرین پال';
+        $inputs['transaction_id'] = random_int(10000, 99999);
+        $inputs['tracking_code'] = 'PEY-' . strtoupper(Str::random(10));
+
+        $user = Auth::user();
+        $wallet = $user->wallet;
+
+        try {
+            
+            DB::beginTransaction();
+
+            $peyment = Peyment::create([
+                'user_id' => $user->id,
+                'amount' => $inputs['amount'],
+                'gateway' => $inputs['gateway'],
+                'transaction_id' => $inputs['transaction_id'],
+                'tracking_code' => $inputs['tracking_code']
+            ]);
+
+            $walletPeyment = WalletPeyment::create([
+                'wallet_id' => $wallet->id,
+                'peyment_id' => $peyment->id,
+                'peyment_object' => json_encode($peyment),
+                'charge_amount' => $peyment->amount,
+                'peyment_status' => 1,
+                'tracking_code' => 'WAL-' . strtoupper(Str::random(10))
+            ]);
+
+            $wallet->update([
+                'amount' => $walletPeyment->charge_amount
+            ]);
+
+            DB::commit();
+            return to_route('user.dashbord.index')->with('success', 'موجودی کیف پول شما افزایش یافت');
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return to_route('user.dashbord.index')->with('error', 'Something went wrong');
+        }
+
+
+    }
+
+
+}
