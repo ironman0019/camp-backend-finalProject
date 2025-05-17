0532d448b2e0327ad3847f2226409908b9259958
Emad Isavi <emadesavi@gmail.com>
2025-05-18 02:59:45 +0330

    Modified: fix admin panel coupan store and update request validation

--- app/Http/Requests/Admin/Coupan/UpdateCoupanRequest.php
+++ app/Http/Requests/Admin/Coupan/UpdateCoupanRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Admin\Coupan;
 
+use Illuminate\Validation\Rule;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateCoupanRequest extends FormRequest
@@ -23,8 +24,12 @@ class UpdateCoupanRequest extends FormRequest
     {
         return [
             'code' => 'required|max:120|min:2|regex:/^[a-zA-Z0-9\., ]+$/u',
-            'amount' => 'required|numeric',
             'amount_type' => 'required|in:0,1',
+            'amount' => [
+                'required',
+                'numeric',
+                Rule::when($this->input('amount_type') == 0, ['min:1', 'max:99'])
+            ],
             'discount_ceiling' => 'required_if:amount_type,0|nullable|numeric',
             'start_date' => 'required|numeric',
             'end_date' => 'required|numeric',
