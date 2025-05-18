e0a10b688c6be4804f842303a5ecf35c735bb825
Emad Isavi <emadesavi@gmail.com>
2025-05-19 01:28:19 +0330

    Modifiedd: add ticketAdmin relation to ticket category model

--- app/Models/Ticket/TicketCategory.php
+++ app/Models/Ticket/TicketCategory.php
@@ -3,6 +3,7 @@
 namespace App\Models\Ticket;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TicketCategory extends Model
@@ -10,6 +11,12 @@ class TicketCategory extends Model
     use SoftDeletes;
 
     protected $fillable = ['name', 'status'];
+
+    // Relation with ticket admins
+    public function ticketAdmins(): HasMany
+    {
+        return $this->hasMany(TicketAdmin::class);
+    }
 
 
 }
